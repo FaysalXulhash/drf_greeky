3895603d427acc790205da1462a5737c1b1b16f2
FaysalXulhash <faysalahammed80@gmail.com>
2024-10-21 21:38:43 +0600

    added modelviewset, router and basicauthentication

--- api/views.py
+++ api/views.py
@@ -4,36 +4,47 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Student
 from .serializers import StudentSerializer
+from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-class StudentApi(APIView):
-    def get(self, request, format=None):
-        students = Student.objects.all()
-        serializer = StudentSerializer(students, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = StudentSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status = status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class Studentviewset(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    # authentication_classes = [BasicAuthentication]
+    # permission_classes = [IsAuthenticated]
 
 
-class studentDetailApi(APIView):
-    def get(self, request, pk, format=None):
-        student = Student.objects.get(id=pk)
-        serializer = StudentSerializer(student)
-        return Response(serializer.data)
+
+# class StudentApi(APIView):
+#     def get(self, request, format=None):
+#         students = Student.objects.all()
+#         serializer = StudentSerializer(students, many=True)
+#         return Response(serializer.data)
+
+#     def post(self, request, format=None):
+#         serializer = StudentSerializer(data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status = status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# class studentDetailApi(APIView):
+#     def get(self, request, pk, format=None):
+#         student = Student.objects.get(id=pk)
+#         serializer = StudentSerializer(student)
+#         return Response(serializer.data)
     
-    def put(self, request, pk, format=None):
-        student = Student.objects.get(id=pk)
-        serialzer = StudentSerializer(student, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status = status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def put(self, request, pk, format=None):
+#         student = Student.objects.get(id=pk)
+#         serialzer = StudentSerializer(student, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status = status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    def delete(self, request, pk ,format=None):
-        student = Student.objects.get(id=pk)
-        student.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     def delete(self, request, pk ,format=None):
+#         student = Student.objects.get(id=pk)
+#         student.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
--- gs_project/urls.py
+++ gs_project/urls.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
-from api.views import StudentApi, studentDetailApi
+#from api.views import StudentApi, studentDetailApi
+from api.views import Studentviewset
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('student', Studentviewset, basename='student')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
-    path('student/', StudentApi.as_view(), name='student'),
-    path('student/<int:pk>/', studentDetailApi.as_view(), name='student-details'),
+    path('', include(router.urls),),
+    #path('student/', StudentApi.as_view(), name='student'),
+    #path('student/<int:pk>/', studentDetailApi.as_view(), name='student-details'),
 ]
+   
