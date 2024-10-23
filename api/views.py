fb060155db5e92b279d77be6cc1e4ecf77b99533
FaysalXulhash <faysalahammed80@gmail.com>
2024-10-23 15:17:44 +0600

    token based authentication implemented

--- api/views.py
+++ api/views.py
@@ -5,14 +5,14 @@ from rest_framework.response import Response
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework import viewsets
-from rest_framework.authentication import BasicAuthentication
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 class Studentviewset(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    # authentication_classes = [BasicAuthentication]
-    # permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 
--- gs_project/urls.py
+++ gs_project/urls.py
@@ -3,15 +3,18 @@ from django.urls import path, include
 #from api.views import StudentApi, studentDetailApi
 from api.views import Studentviewset
 from rest_framework.routers import DefaultRouter
-
+from rest_framework.authtoken import views #for get/create token
+from api.auth import CustomAuthToken
 router = DefaultRouter()
 router.register('student', Studentviewset, basename='student')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-
     path('', include(router.urls),),
+    #path('api-token/', views.obtain_auth_token)
+    #path('api-token/', CustomAuthToken.as_view()), # for custom ObtainAuthToken
+
     #path('student/', StudentApi.as_view(), name='student'),
     #path('student/<int:pk>/', studentDetailApi.as_view(), name='student-details'),
 ]
